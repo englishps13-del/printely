bc8eb91cf03cac26df98063adc64ed3cec010cca
englishps13-del <englishps13@gmail.com>
2025-09-20 11:06:13 +0100

    added ms1

--- public/ms1_1.js
+++ public/ms1_1.js
@@ -1,5 +1,3 @@
-<script type="module">
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 import { getFirestore, collection, addDoc, serverTimestamp, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
